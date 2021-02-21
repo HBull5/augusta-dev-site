f962997dcb0030c25b77fcba1efb87673ece8f19
Harcourt Bull <harcourtbull@Harcourts-MBP.attlocal.net>
2021-02-21 01:13:09 -0500

    minimize, maximize, & close working

--- public/scripts/draggable.js
+++ public/scripts/draggable.js
@@ -1,6 +1,12 @@
 const dragArea = document.querySelector('.fakeMenu');
 const dragItem = document.querySelector('.drag-item');
 const container = document.querySelector('.drag-container');
+const closeBtn = document.querySelector('.fakeClose');
+const minimizeBtn = document.querySelector('.fakeMinimize');
+const maximizeBtn = document.querySelector('.fakeZoom');
+const shortcut1 = document.querySelector('.shortcut-1');
+const shortcut2 = document.querySelector('.shortcut-2');
+const shortcut3 = document.querySelector('.shortcut-3');
 let active = false;
 let currentX;
 let currentY;
@@ -8,6 +14,14 @@ let initialX;
 let initialY;
 let xOffset = 0;
 let yOffset = 0;
+
+function sleep(ms) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve();
+		}, ms);
+	});
+}
 
 function dragStart(e) {
 	if (e.type === 'touchstart') {
@@ -48,12 +62,27 @@ function drag(e) {
 	}
 }
 
+function setTranslate(xPos, yPos, el) {
+	el.style.transform = 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0)';
+}
+
 function offScreen(e) {
 	active = false;
 }
 
-function setTranslate(xPos, yPos, el) {
-	el.style.transform = 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0)';
+function fullscreen(e) {
+	dragItem.classList.toggle('fullscreen');
+}
+
+function close(e) {
+	if (dragItem.classList.contains('fullscreen')) {
+		dragItem.classList.remove('fullscreen');
+	}
+	dragItem.classList.add('minimize');
+}
+
+async function newTerminal(e) {
+	dragItem.classList.remove('minimize');
 }
 
 container.addEventListener('touchstart', dragStart, false);
@@ -64,3 +93,8 @@ container.addEventListener('mousedown', dragStart, false);
 container.addEventListener('mouseup', dragEnd, false);
 container.addEventListener('mousemove', drag, false);
 container.addEventListener('mouseleave', offScreen, false);
+
+closeBtn.addEventListener('click', close, false);
+minimizeBtn.addEventListener('click', close, false);
+shortcut1.addEventListener('click', newTerminal, false);
+maximizeBtn.addEventListener('click', fullscreen, false);
